be1ed0dd411f41bd2b736b57553e85464a0fc033
José Luis Gutierrez <joselugz03@gmail.com>
2024-06-20 18:47:43 -0300

    update themes

--- app/src/main/java/com/resucito/app/ui/theme/Theme.kt
+++ app/src/main/java/com/resucito/app/ui/theme/Theme.kt
@@ -4,67 +4,95 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+val LightColorPalette = lightColorScheme(
+    primary = LightModeColors.primary,
+    onPrimary = LightModeColors.onPrimary,
+    primaryContainer = LightModeColors.primaryContainer,
+    onPrimaryContainer = LightModeColors.onPrimaryContainer,
+    secondary = LightModeColors.secondary,
+    onSecondary = LightModeColors.onSecondary,
+    secondaryContainer = LightModeColors.secondaryContainer,
+    onSecondaryContainer = LightModeColors.onSecondaryContainer,
+    tertiary = LightModeColors.tertiary,
+    onTertiary = LightModeColors.onTertiary,
+    tertiaryContainer = LightModeColors.tertiaryContainer,
+    onTertiaryContainer = LightModeColors.onTertiaryContainer,
+    error = LightModeColors.error,
+    onError = LightModeColors.onError,
+    errorContainer = LightModeColors.errorContainer,
+    onErrorContainer = LightModeColors.onErrorContainer,
+    background = LightModeColors.background,
+    onBackground = LightModeColors.onBackground,
+    surface = LightModeColors.surface,
+    onSurface = LightModeColors.onSurface,
+    surfaceVariant = LightModeColors.surfaceVariant,
+    onSurfaceVariant = LightModeColors.onSurfaceVariant,
+    outline = LightModeColors.outline,
+    outlineVariant = LightModeColors.outlineVariant
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+val DarkColorPalette = darkColorScheme(
+    primary = DarkModeColors.primary,
+    onPrimary = DarkModeColors.onPrimary,
+    primaryContainer = DarkModeColors.primaryContainer,
+    onPrimaryContainer = DarkModeColors.onPrimaryContainer,
+    secondary = DarkModeColors.secondary,
+    onSecondary = DarkModeColors.onSecondary,
+    secondaryContainer = DarkModeColors.secondaryContainer,
+    onSecondaryContainer = DarkModeColors.onSecondaryContainer,
+    tertiary = DarkModeColors.tertiary,
+    onTertiary = DarkModeColors.onTertiary,
+    tertiaryContainer = DarkModeColors.tertiaryContainer,
+    onTertiaryContainer = DarkModeColors.onTertiaryContainer,
+    error = DarkModeColors.error,
+    onError = DarkModeColors.onError,
+    errorContainer = DarkModeColors.errorContainer,
+    onErrorContainer = DarkModeColors.onErrorContainer,
+    background = DarkModeColors.background,
+    onBackground = DarkModeColors.onBackground,
+    surface = DarkModeColors.surface,
+    onSurface = DarkModeColors.onSurface,
+    surfaceVariant = DarkModeColors.surfaceVariant,
+    onSurfaceVariant = DarkModeColors.onSurfaceVariant,
+    outline = DarkModeColors.outline,
+    outlineVariant = DarkModeColors.outlineVariant
 )
 
 @Composable
 fun ResucitoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable() () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
+    val colorScheme = if (darkTheme) DarkColorPalette else LightColorPalette
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = AppTypography,
+        shapes = Shapes(),
         content = content
     )
 }
