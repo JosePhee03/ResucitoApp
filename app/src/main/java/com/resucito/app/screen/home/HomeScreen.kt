b8c54795c0cef35ccf4475460c021dfebad8db88
José Luis Gutierrez <joselugz03@gmail.com>
2024-06-23 22:16:08 -0300

    update home screen

--- app/src/main/java/com/resucito/app/screen/home/HomeScreen.kt
+++ app/src/main/java/com/resucito/app/screen/home/HomeScreen.kt
@@ -1,85 +1,129 @@
 package com.resucito.app.screen.home
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Share
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.resucito.app.R
+import com.resucito.app.data.Category
+import com.resucito.app.data.Stage
+import com.resucito.app.navigation.Search
+import com.resucito.app.screen.home.componets.CardCategory
+import com.resucito.app.screen.home.componets.CardStage
+import com.resucito.app.util.colorStage
+import com.resucito.app.viewmodel.AppPreferencesProvider
+import java.util.Locale
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview
-fun HomeScreen() {
+fun HomeScreen(
+    navController: NavHostController,
+    appPreferencesProvider: AppPreferencesProvider,
+    isDarkTheme: Boolean,
+    onToggleTheme: (Boolean) -> Unit
+) {
 
-    Column(modifier = Modifier.fillMaxWidth()) {
+    Column(modifier = Modifier.fillMaxSize()) {
         TopAppBar(title = {
             Text(
                 text = "Resucitó",
                 color = MaterialTheme.colorScheme.primary,
                 fontWeight = FontWeight.Bold
             )
-        }, actions = { IconButton(onClick = { /*TODO*/ }) { Icon(
-            imageVector = Icons.Default.Share,
-            contentDescription = "Cambiar el color del tema"
-        )}})
-        Text(text = "Índice de cantos", textAlign = TextAlign.Center, style = MaterialTheme.typography.headlineMedium)
-        Text(text = "Por etapa", style = MaterialTheme.typography.titleLarge)
-        CardPreview()
+        }, actions = {
+            IconButton(onClick = { onToggleTheme(!isDarkTheme) }) {
+                Icon(
+                    painter = painterResource(if (isDarkTheme) R.drawable.ic_sun else R.drawable.ic_moon),
+                    contentDescription = "Cambiar el color del tema"
+                )
+            }
+        })
+        LazyVerticalGrid(
+            modifier = Modifier.fillMaxSize(),
+            columns = GridCells.Adaptive(160.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp), // Espacio vertical entre elementos
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Índice de cantos",
+                        textAlign = TextAlign.Center,
+                        style = MaterialTheme.typography.headlineMedium
+                    )
+                }
+            }
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                Text(
+                    modifier = Modifier.padding(top = 16.dp),
+                    text = "Etapas del camino",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Medium
+
+                )
+            }
+
+            Stage.entries.forEach {
+                item {
+                    CardStage(
+                        it.name.lowercase(Locale.ENGLISH),
+                        appPreferencesProvider.getStagePreferences(it),
+                        colorStage(it).backgroundColor
+                    ) {
+                        navController.navigate(Search(it.name, null))
+                    }
+                }
+            }
+
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                Text(
+                    modifier = Modifier.padding(top = 16.dp),
+                    text = "Índice litúrgico",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.Medium
+
+                )
+            }
+
+            Category.entries.forEach {
+                item {
+                    CardCategory(
+                        it.name.lowercase(Locale.ENGLISH),
+                        appPreferencesProvider.getCategoryPreferences(it)
+                    ) {
+                        navController.navigate(Search(null, it.name))
+                    }
+                }
+            }
+        }
     }
 }
 
-@Composable
-@Preview(showBackground = true)
-fun CardPreview () {
-    ElevatedCard(
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.background,
-            disabledContainerColor = MaterialTheme.colorScheme.background
-        ),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 6.dp
-        ),
-        modifier = Modifier
-            .width(240.dp).background(MaterialTheme.colorScheme.background)
-    ) {
-        Box(modifier = Modifier
-            .fillMaxWidth()
-            .height(60.dp)
-            .background(colorResource(id = R.color.precatechumenateContainer)))
-        Text(
-            text = "Precatecumenado",
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.titleLarge
-        )
-        Text(
-            text = "123 Cantos",
-            textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.titleSmall
-        )
-    }
-}
+
