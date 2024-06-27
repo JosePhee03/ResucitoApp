a4fced720cd65599f8ac56424b40d6a8d5d74079
José Luis Gutierrez <joselugz03@gmail.com>
2024-06-26 23:20:59 -0300

    add topbar and item album in album screen

--- app/src/main/java/com/resucito/app/presentation/ui/screen/album/AlbumScreen.kt
+++ app/src/main/java/com/resucito/app/presentation/ui/screen/album/AlbumScreen.kt
@@ -1,15 +1,31 @@
 package com.resucito.app.presentation.ui.screen.album
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.resucito.app.presentation.ui.screen.album.components.ItemAlbumSongs
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview
 fun AlbumScreen () {
     
     Column {
-        Text(text = "Lista de Cantos")
+        TopAppBar(title = { Text("Albumes") })
+        LazyColumn {
+            item {
+                ItemAlbumSongs("Favoritos", 100)
+            }
+        }
     }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun AlbumScreenPreview () {
+
+    AlbumScreen()
 }
