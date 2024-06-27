5d9f0cf6925fa3e2b5a0ee87f6e0822f94f4d651
José Luis Gutierrez <joselugz03@gmail.com>
2024-06-27 12:28:03 -0300

    add albumRepository to UpdateFavoriteSongUseCase

--- app/src/main/java/com/resucito/app/domain/usecase/UpdateFavoriteSongUseCase.kt
+++ app/src/main/java/com/resucito/app/domain/usecase/UpdateFavoriteSongUseCase.kt
@@ -1,14 +1,42 @@
 package com.resucito.app.domain.usecase
 
-import com.resucito.app.data.preferences.SongSharedPreferences
+import com.resucito.app.data.repository.AlbumRepository
 import com.resucito.app.data.repository.SongRepository
-import com.resucito.app.domain.model.Category
+import com.resucito.app.domain.model.Album
 import javax.inject.Inject
 
-class UpdateFavoriteSongUseCase @Inject constructor(private val songRepository: SongRepository) {
+class UpdateFavoriteSongUseCase @Inject constructor(
+    private val songRepository: SongRepository,
+    private val albumRepository: AlbumRepository
+) {
 
-    suspend fun execute(songId: String, favorite: Boolean) {
-        songRepository.updateFavoriteSong(songId, favorite)
+
+   private val favoriteAlbumName = "Favoritos"
+
+    suspend fun execute(songId: String, favorite: Boolean): Result<Unit> {
+        return runCatching {
+            var albumId: Int? = null
+
+            if (favorite) {
+                var newAlbumId = albumRepository.getAlbumId(favoriteAlbumName).getOrThrow()
+
+                if (newAlbumId === null) {
+                    newAlbumId = albumRepository.insertAlbum(Album(favoriteAlbumName)).getOrThrow()
+                }
+                albumId = newAlbumId
+            }
+
+            albumRepository.updateAlbumId(songId, albumId).getOrThrow()
+
+            songRepository.updateFavoriteSong(songId, favorite).getOrThrow()
+        }.fold(
+            onSuccess = {
+                Result.success(Unit)
+            },
+            onFailure = {
+                Result.failure(it)
+            }
+        )
     }
 
 }
