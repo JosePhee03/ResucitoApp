d13f943ef206300eaaf92f254e5819b2aa22d9c8
José Luis Gutierrez <joselugz03@gmail.com>
2024-06-26 23:46:23 -0300

    add queries

--- app/src/main/java/com/resucito/app/data/local/dao/AlbumDao.kt
+++ app/src/main/java/com/resucito/app/data/local/dao/AlbumDao.kt
@@ -1,7 +1,30 @@
 package com.resucito.app.data.local.dao
 
-class AlbumDao {
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+import com.resucito.app.data.local.entity.AlbumEntity
+import com.resucito.app.data.local.entity.AlbumSongsRelation
+import com.resucito.app.data.local.entity.SongEntity
 
+interface AlbumDao {
 
+    @Insert
+    suspend fun insertAlbum(album: AlbumEntity): Long
+
+    @Insert
+    suspend fun insertSong(song: SongEntity): Long
+
+    @Transaction
+    suspend fun insertAlbumWithSongs(album: AlbumEntity, songs: List<SongEntity>) {
+        val albumId = insertAlbum(album)
+        for (song in songs) {
+            insertSong(song.copy(albumId = albumId.toInt()))
+        }
+    }
+
+    @Transaction
+    @Query("SELECT * FROM album WHERE id = :albumId")
+    suspend fun getAlbumWithSongs(albumId: Int): List<AlbumSongsRelation>
 
 }
