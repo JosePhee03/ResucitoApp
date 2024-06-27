1f7c5fd8a71003df78fda22469501bdac0e08b73
José Luis Gutierrez <joselugz03@gmail.com>
2024-06-27 12:25:30 -0300

    remove id to AlbumMapper

--- app/src/main/java/com/resucito/app/data/mapper/AlbumMapper.kt
+++ app/src/main/java/com/resucito/app/data/mapper/AlbumMapper.kt
@@ -3,18 +3,16 @@ package com.resucito.app.data.mapper
 import com.resucito.app.data.local.entity.AlbumEntity
 import com.resucito.app.domain.model.Album
 
-class AlbumMapper {
+object AlbumMapper {
 
     fun fromDomainToEntity(album: Album): AlbumEntity {
         return AlbumEntity(
-            id = album.id,
             name = album.name
         )
     }
 
     fun fromEntityToDomain(albumEntity: AlbumEntity): Album {
         return Album(
-            id = albumEntity.id,
             name = albumEntity.name
         )
     }
