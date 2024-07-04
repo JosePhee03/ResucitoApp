a1ff50e1eee08b46cc1b4ae60e5bc34977b49a20
José Luis Gutierrez <joselugz03@gmail.com>
2024-07-04 11:41:57 -0300

    create list song book content

---
+++ app/src/main/java/com/resucito/app/presentation/ui/screen/songbook/components/ListSongBookContent.kt
@@ -0,0 +1,48 @@
+package com.resucito.app.presentation.ui.screen.songbook.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.resucito.app.domain.model.Song
+import com.resucito.app.presentation.ui.screen.search.components.ItemSearchSong
+
+@Composable
+fun ListSongBookContent(
+    scrollState: LazyListState,
+    songs: List<Song>,
+    onClickItem: (String) -> Unit,
+    onChangeFavorite: (String, Boolean) -> Unit,
+) {
+    LazyColumn(state = scrollState) {
+        items(songs, key = { it.id }) {
+            var visible by remember { mutableStateOf(true) }
+
+            AnimatedVisibility(
+                visible = visible,
+                enter = fadeIn(animationSpec = tween(durationMillis = 200)),
+                exit = fadeOut(animationSpec = tween(durationMillis = 200))
+            ) {
+                ItemSearchSong(
+                    page = it.page.toString(),
+                    title = it.title,
+                    subtitle = it.subtitle,
+                    favorite = it.favorite,
+                    stage = it.stage,
+                    onClickItem = { onClickItem(it.id) },
+                    onChangeFavorite = { isFavorite ->
+                        visible = false
+                        onChangeFavorite(it.id, isFavorite)
+                    })
+            }
+        }
+    }
+}
