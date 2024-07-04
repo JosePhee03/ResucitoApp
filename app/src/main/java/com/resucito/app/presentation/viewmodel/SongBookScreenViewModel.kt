876b11c45970ec12d4fdaba3c56d845acbe63ef0
José Luis Gutierrez <joselugz03@gmail.com>
2024-07-04 11:43:24 -0300

    add switch favorite song use case

--- app/src/main/java/com/resucito/app/presentation/viewmodel/SongBookScreenViewModel.kt
+++ app/src/main/java/com/resucito/app/presentation/viewmodel/SongBookScreenViewModel.kt
@@ -8,13 +8,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.resucito.app.domain.model.Song
 import com.resucito.app.domain.usecase.GetAllFavoriteSongsUseCase
+import com.resucito.app.domain.usecase.UpdateFavoriteSongUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SongBookScreenViewModel @Inject constructor(
-    private val getAllFavoriteSongsUseCase: GetAllFavoriteSongsUseCase
+    private val getAllFavoriteSongsUseCase: GetAllFavoriteSongsUseCase,
+    private val updateFavoriteSongUseCase: UpdateFavoriteSongUseCase
 ): ViewModel() {
 
     var isError by mutableStateOf(false)
@@ -25,6 +28,8 @@ class SongBookScreenViewModel @Inject constructor(
 
     private val _favoriteSongs = mutableStateListOf<Song>()
     val favoriteSongs get() = _favoriteSongs.toList()
+
+    var snackBarText by mutableStateOf<String?>(null)
 
     fun getAllFavoriteSongs() {
         viewModelScope.launch {
@@ -40,6 +45,18 @@ class SongBookScreenViewModel @Inject constructor(
             )
 
             isLoading = false
+        }
+    }
+
+    fun switchFavoriteSong (songId: String, favorite: Boolean) {
+        viewModelScope.launch {
+            snackBarText = null
+            delay(400)
+            _favoriteSongs.removeIf { it.id == songId }
+            updateFavoriteSongUseCase.execute(songId, favorite).fold(
+                onSuccess = {snackBarText = if (favorite) "Canto Guardado en el album de \"Favoritos\"" else "Se quito el canto del album de \"Favoritos\""},
+                onFailure = { snackBarText = it.message ?: "Error favorito"}
+            )
         }
     }
 
