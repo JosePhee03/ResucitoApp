286e6cd4c3625ad689766b1a43b7ddc96dec3d79
José Luis Gutierrez <joselugz03@gmail.com>
2024-07-04 11:40:42 -0300

    create empty layout

---
+++ app/src/main/java/com/resucito/app/presentation/ui/components/EmptyLayout.kt
@@ -0,0 +1,56 @@
+package com.resucito.app.presentation.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun EmptyLayout(
+    paddingValues: PaddingValues,
+    text: String,
+    buttonText: String,
+    onReset: () -> Unit,
+) {
+
+    Box(
+        modifier = Modifier
+            .padding(paddingValues)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(horizontal = 32.dp)
+        ) {
+            Text(
+                text = text,
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                fontWeight = FontWeight.Medium
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { onReset() }, colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    contentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            ) {
+                Text(text = buttonText, style = MaterialTheme.typography.titleMedium)
+            }
+        }
+    }
+}
