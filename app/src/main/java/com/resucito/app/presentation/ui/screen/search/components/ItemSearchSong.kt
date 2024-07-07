4a821bdcd8210331ee02c825544ff70ac1befeff
José Luis Gutierrez <joselugz03@gmail.com>
2024-07-06 21:02:37 -0300

    remove unused code

--- app/src/main/java/com/resucito/app/presentation/ui/screen/search/components/ItemSearchSong.kt
+++ app/src/main/java/com/resucito/app/presentation/ui/screen/search/components/ItemSearchSong.kt
@@ -1,9 +1,5 @@
 package com.resucito.app.presentation.ui.screen.search.components
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,10 +11,6 @@ import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,37 +32,25 @@ fun ItemSearchSong(
 
     val (_, backgroundColor) = colorStage(stage)
 
-    var visible by remember { mutableStateOf(true) }
-
-    if (visible) {
-        AnimatedVisibility(
-            visible = visible,
-            enter = fadeIn(animationSpec = tween(durationMillis = 300)),
-            exit = fadeOut(animationSpec = tween(durationMillis = 300))
-        ) {
-        }
-        ListItem(
-            modifier = Modifier
-                .fillMaxWidth()
-                .clickable { onClickItem() },
-            headlineContent = { Text(title) },
-            supportingContent = {
-                Text(subtitle)
-            },
-            leadingContent = {
-                Text(
-                    text = page,
-                    modifier = Modifier
-                        .clip(MaterialTheme.shapes.small)
-                        .background(backgroundColor)
-                        .padding(8.dp),
-                )
-            },
-            trailingContent = { FavoriteIconButton(favorite, onChangeFavorite) }
-        )
-
-    }
-
+    ListItem(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClickItem() },
+        headlineContent = { Text(title) },
+        supportingContent = {
+            Text(subtitle)
+        },
+        leadingContent = {
+            Text(
+                text = page,
+                modifier = Modifier
+                    .clip(MaterialTheme.shapes.small)
+                    .background(backgroundColor)
+                    .padding(8.dp),
+            )
+        },
+        trailingContent = { FavoriteIconButton(favorite, onChangeFavorite) }
+    )
 
 }
 
