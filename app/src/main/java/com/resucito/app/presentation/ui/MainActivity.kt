4366ae1c632f24f0a2aafade202d3b1fb064b87e
José Luis Gutierrez <joselugz03@gmail.com>
2024-07-11 17:17:50 -0300

    refactor main activity

--- app/src/main/java/com/resucito/app/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/resucito/app/presentation/ui/MainActivity.kt
@@ -12,30 +12,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
 import com.resucito.app.presentation.ui.components.NavigationBottomBar
 import com.resucito.app.presentation.ui.components.NavigationStatusBar
-import com.resucito.app.presentation.ui.navigation.Routes
-import com.resucito.app.presentation.ui.screen.home.HomeScreen
-import com.resucito.app.presentation.ui.screen.library.LibraryScreen
-import com.resucito.app.presentation.ui.screen.more.MoreScreen
-import com.resucito.app.presentation.ui.screen.search.SearchScreen
-import com.resucito.app.presentation.ui.screen.song.SongScreen
-import com.resucito.app.presentation.ui.screen.songbook.SongBookScreen
-import com.resucito.app.presentation.ui.screen.start.StartScreen
+import com.resucito.app.presentation.ui.navigation.NavigationScreen
 import com.resucito.app.presentation.ui.theme.ResucitoTheme
+import com.resucito.app.presentation.viewmodel.ApplicationState
 import com.resucito.app.presentation.viewmodel.ApplicationViewModel
 import com.resucito.app.presentation.viewmodel.HomeScreenViewModel
 import com.resucito.app.presentation.viewmodel.LibraryScreenViewModel
 import com.resucito.app.presentation.viewmodel.SearchScreenViewModel
-import com.resucito.app.presentation.viewmodel.SongBookScreenViewModel
 import com.resucito.app.presentation.viewmodel.SongScreenViewModel
 import com.resucito.app.presentation.viewmodel.StartScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -43,151 +33,71 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val appViewModel: ApplicationViewModel by viewModels()
+    private val applicationViewModel: ApplicationViewModel by viewModels()
+    private val startScreenViewModel: StartScreenViewModel by viewModels()
+    private val homeScreenViewModel: HomeScreenViewModel by viewModels()
+    private val searchScreenViewModel: SearchScreenViewModel by viewModels()
+    private val songScreenViewModel: SongScreenViewModel by viewModels()
+    private val libraryScreenViewModel: LibraryScreenViewModel by viewModels()
+    private lateinit var applicationUiState: ApplicationState
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val screenSplash = installSplashScreen()
         super.onCreate(savedInstanceState)
         screenSplash.setKeepOnScreenCondition { false }
         setContent {
+            val isSystemInDarkTheme = isSystemInDarkTheme()
 
-            appViewModel.onCreate(isSystemInDarkTheme())
+            applicationUiState = applicationViewModel.state.collectAsState(
+                initial = ApplicationState(
+                    isDarkTheme = applicationViewModel.getIsDarkTheme(isSystemInDarkTheme),
+                    isFirstRun = applicationViewModel.getIsFirstRun()
+                )
+            ).value
 
-            ResucitoTheme(appViewModel.isDarkTheme) {
-                MainScreen(
-                    isFirstRun = appViewModel.getIsFirstRun(),
-                    onToggleFirstRun = { appViewModel.setIsFirstRun(it) },
-                    isDarkTheme = appViewModel.isDarkTheme,
-                    onToggleTheme = { appViewModel.setIsDarkMode(it) })
+            ResucitoTheme(applicationUiState.isDarkTheme) {
+                MainScreen()
             }
         }
     }
 
-}
+    @Composable
+    fun MainScreen() {
 
-@Composable
-fun MainScreen(
-    isFirstRun: Boolean,
-    onToggleFirstRun: (Boolean) -> Unit,
-    isDarkTheme: Boolean,
-    onToggleTheme: (Boolean) -> Unit
-) {
+        val navController = rememberNavController()
+        val snackBarController = remember { SnackbarHostState() }
 
-    val navController = rememberNavController()
-    val snackBarController = remember { SnackbarHostState() }
+        NavigationStatusBar(
+            navController, applicationUiState.isDarkTheme
+        )
 
-    NavigationStatusBar(
-        navController,
-        isDarkTheme
-    )
-
-    Scaffold(
-        snackbarHost = {
-            SnackbarHost(hostState = snackBarController)
-        },
-        bottomBar = { NavigationBottomBar(navController) },
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .padding(paddingValues)
-                .fillMaxSize()
-        ) {
-            NavHost(
-                navController,
-                startDestination = if (isFirstRun) Routes.Start else Routes.Home
+        Scaffold(
+            snackbarHost = {
+                SnackbarHost(hostState = snackBarController)
+            },
+            bottomBar = { NavigationBottomBar(navController) },
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize()
             ) {
-                composable<Routes.Start> {
-                    val viewModel: StartScreenViewModel = hiltViewModel()
-                    StartScreen(
-                        navigateToHome = { navController.navigate(Routes.Home) },
-                        onRemoveStack = { navController.popBackStack() },
-                        onCreate = viewModel::onCreate,
-                        isLoading = viewModel.isLoading,
-                        isError = viewModel.isError,
-                        onToggleFirstRun = onToggleFirstRun
-                    )
-                }
-                composable<Routes.Home> {
-                    val viewModel: HomeScreenViewModel = hiltViewModel()
-                    HomeScreen(
-                        navigateToSearch = { stageId, categoryId ->
-                            navController.navigate(
-                                Routes.Search(
-                                    stageId,
-                                    categoryId
-                                )
-                            )
-                        },
-                        isDarkTheme = isDarkTheme,
-                        onToggleTheme = onToggleTheme,
-                        getCountStage = viewModel::getCountStage,
-                        getCountCategory = viewModel::getCountCategory
-                    )
-                }
-                composable<Routes.Search> { navBackStackEntry ->
-                    val (stageId, categoryId) = navBackStackEntry.toRoute<Routes.Search>()
-
-                    val viewModel: SearchScreenViewModel = hiltViewModel()
-
-                    SearchScreen(
-                        navigateToSong = { navController.navigate(Routes.Song(it)) },
-                        stageId = stageId,
-                        categoryId = categoryId,
-                        songs = viewModel.songs,
-                        filters = viewModel.filters,
-                        isLoading = viewModel.isLoading,
-                        searchSong = viewModel::searchSong,
-                        setSearchFilter = viewModel::setSearchFilters,
-                        switchFavoriteSong = viewModel::switchFavoriteSong,
-                        snackBarText = viewModel.snackBarText,
-                        snackBarController = snackBarController
-                    )
-                }
-                composable<Routes.Song> { backStackEntry ->
-                    val songRoute: Routes.Song = backStackEntry.toRoute()
-
-                    val viewModel: SongScreenViewModel = hiltViewModel()
-
-                    SongScreen(
-                        navController = navController,
-                        snackBarController = snackBarController,
-                        song = viewModel.song,
-                        isLoading = viewModel.isLoading,
-                        isError = viewModel.isError,
-                        findSong = viewModel::findSongById,
-                        songId = songRoute.id,
-                        onChangeFavorite = viewModel::switchFavoriteSong,
-                        snackBarText = viewModel.snackBarText
-                    )
-                }
-                composable<Routes.Library> {
-                    val viewModel: LibraryScreenViewModel = hiltViewModel()
-                    LibraryScreen(
-                        navigateToSongBook = { navController.navigate(Routes.SongBook) },
-                        getAlbums = viewModel::getAllFavoriteSongs,
-                        isLoading = viewModel.isLoading,
-                        isError = viewModel.isError,
-                        favoriteSongs = viewModel.favoriteSongs
-                    )
-                }
-                composable<Routes.SongBook> {
-                    val viewModel: SongBookScreenViewModel = hiltViewModel()
-                    SongBookScreen(
-                        uiState = viewModel.uiState,
-                        onBackNavigate = { navController.navigateUp() },
-                        getSongBook = viewModel::getAllFavoriteSongs,
-                        navigateToSong = { songId -> navController.navigate(Routes.Song(songId)) },
-                        onChangeFavorite = viewModel::switchFavoriteSong
-                    )
-                }
-                composable<Routes.More> {
-                    MoreScreen(
-                        isDarkTheme = isDarkTheme,
-                        onToggleTheme = onToggleTheme
-                    )
-                }
+                NavigationScreen(
+                    navController = navController,
+                    snackBarController = snackBarController,
+                    applicationViewModel = applicationViewModel,
+                    applicationState = applicationUiState,
+                    startScreenViewModel = startScreenViewModel,
+                    homeScreenViewModel = homeScreenViewModel,
+                    searchScreenViewModel = searchScreenViewModel,
+                    songScreenViewModel = songScreenViewModel,
+                    libraryScreenViewModel = libraryScreenViewModel
+                )
             }
         }
     }
+
 }
+
+
 
---
+++ app/src/main/java/com/resucito/app/presentation/ui/navigation/NavigationScreen.kt
@@ -0,0 +1,138 @@
+package com.resucito.app.presentation.ui.navigation
+
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import com.resucito.app.presentation.ui.screen.home.HomeScreen
+import com.resucito.app.presentation.ui.screen.library.LibraryScreen
+import com.resucito.app.presentation.ui.screen.more.MoreScreen
+import com.resucito.app.presentation.ui.screen.search.SearchScreen
+import com.resucito.app.presentation.ui.screen.song.SongScreen
+import com.resucito.app.presentation.ui.screen.songbook.SongBookScreen
+import com.resucito.app.presentation.ui.screen.start.StartScreen
+import com.resucito.app.presentation.viewmodel.ApplicationState
+import com.resucito.app.presentation.viewmodel.ApplicationViewModel
+import com.resucito.app.presentation.viewmodel.HomeScreenViewModel
+import com.resucito.app.presentation.viewmodel.LibraryScreenViewModel
+import com.resucito.app.presentation.viewmodel.SearchScreenViewModel
+import com.resucito.app.presentation.viewmodel.SongBookScreenViewModel
+import com.resucito.app.presentation.viewmodel.SongScreenViewModel
+import com.resucito.app.presentation.viewmodel.StartScreenViewModel
+
+@Composable
+fun NavigationScreen (
+    navController: NavHostController,
+    snackBarController: SnackbarHostState,
+    applicationViewModel: ApplicationViewModel,
+    applicationState: ApplicationState,
+    startScreenViewModel: StartScreenViewModel,
+    homeScreenViewModel: HomeScreenViewModel,
+    searchScreenViewModel: SearchScreenViewModel,
+    songScreenViewModel: SongScreenViewModel,
+    libraryScreenViewModel: LibraryScreenViewModel
+) {
+
+    val isFirstRun = applicationState.isFirstRun
+    val isDarkTheme = applicationState.isDarkTheme
+    val onToggleFirstRun = remember<(Boolean) -> Unit>{
+        { applicationViewModel.setIsFirstRun(it) }
+    }
+    val onToggleTheme = remember<(Boolean) -> Unit>{
+        { applicationViewModel.setIsDarkMode(it) }
+    }
+
+
+    NavHost(
+        navController,
+        startDestination = if (isFirstRun) Routes.Start else Routes.Home
+    ) {
+        composable<Routes.Start> {
+            StartScreen(
+                navigateToHome = { navController.navigate(Routes.Home) },
+                onRemoveStack = { navController.popBackStack() },
+                onCreate = startScreenViewModel::onCreate,
+                isLoading = startScreenViewModel.isLoading,
+                isError = startScreenViewModel.isError,
+                onToggleFirstRun = onToggleFirstRun
+            )
+        }
+        composable<Routes.Home> {
+            HomeScreen(
+                navigateToSearch = { stageId, categoryId ->
+                    navController.navigate(
+                        Routes.Search(
+                            stageId,
+                            categoryId
+                        )
+                    )
+                },
+                isDarkTheme = isDarkTheme,
+                onToggleTheme = onToggleTheme,
+                getCountStage = homeScreenViewModel::getCountStage,
+                getCountCategory = homeScreenViewModel::getCountCategory
+            )
+        }
+        composable<Routes.Search> { navBackStackEntry ->
+            val (stageId, categoryId) = navBackStackEntry.toRoute<Routes.Search>()
+
+            SearchScreen(
+                navigateToSong = { navController.navigate(Routes.Song(it)) },
+                stageId = stageId,
+                categoryId = categoryId,
+                songs = searchScreenViewModel.songs,
+                filters = searchScreenViewModel.filters,
+                isLoading = searchScreenViewModel.isLoading,
+                searchSong = searchScreenViewModel::searchSong,
+                setSearchFilter = searchScreenViewModel::setSearchFilters,
+                switchFavoriteSong = searchScreenViewModel::switchFavoriteSong,
+                snackBarText = searchScreenViewModel.snackBarText,
+                snackBarController = snackBarController
+            )
+        }
+        composable<Routes.Song> { backStackEntry ->
+            val songRoute: Routes.Song = backStackEntry.toRoute()
+
+            SongScreen(
+                navController = navController,
+                snackBarController = snackBarController,
+                song = songScreenViewModel.song,
+                isLoading = songScreenViewModel.isLoading,
+                isError = songScreenViewModel.isError,
+                findSong = songScreenViewModel::findSongById,
+                songId = songRoute.id,
+                onChangeFavorite = songScreenViewModel::switchFavoriteSong,
+                snackBarText = songScreenViewModel.snackBarText
+            )
+        }
+        composable<Routes.Library> {
+            LibraryScreen(
+                navigateToSongBook = { navController.navigate(Routes.SongBook) },
+                getAlbums = libraryScreenViewModel::getAllFavoriteSongs,
+                isLoading = libraryScreenViewModel.isLoading,
+                isError = libraryScreenViewModel.isError,
+                favoriteSongs = libraryScreenViewModel.favoriteSongs
+            )
+        }
+        composable<Routes.SongBook> {
+            val viewModel: SongBookScreenViewModel = hiltViewModel()
+            SongBookScreen(
+                uiState = viewModel.uiState,
+                onBackNavigate = { navController.navigateUp() },
+                getSongBook = viewModel::getAllFavoriteSongs,
+                navigateToSong = { songId -> navController.navigate(Routes.Song(songId)) },
+                onChangeFavorite = viewModel::switchFavoriteSong
+            )
+        }
+        composable<Routes.More> {
+            MoreScreen(
+                isDarkTheme = isDarkTheme,
+                onToggleTheme = onToggleTheme
+            )
+        }
+    }
+}
