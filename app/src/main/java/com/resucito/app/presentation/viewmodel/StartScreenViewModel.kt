b3cf359775e18e03d8ce747fce2f63c8ee71f6f8
José Luis Gutierrez <joselugz03@gmail.com>
2024-07-11 17:55:08 -0300

    create ui state with mutable state flow

--- app/src/main/java/com/resucito/app/presentation/viewmodel/StartScreenViewModel.kt
+++ app/src/main/java/com/resucito/app/presentation/viewmodel/StartScreenViewModel.kt
@@ -1,9 +1,6 @@
 package com.resucito.app.presentation.viewmodel
 
 import android.content.Context
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.resucito.app.domain.model.Category
@@ -14,8 +11,13 @@ import com.resucito.app.domain.usecase.InsertSongsUseCase
 import com.resucito.app.domain.usecase.SetCountCategoryUseCase
 import com.resucito.app.domain.usecase.SetCountStageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class StartState(
+    val isLoading: Boolean = true, val isError: Boolean = false
+)
 
 @HiltViewModel
 class StartScreenViewModel @Inject constructor(
@@ -26,44 +28,42 @@ class StartScreenViewModel @Inject constructor(
     private val cleanDatabasesUseCase: CleanDatabasesUseCase
 ) : ViewModel() {
 
-    var isLoading by mutableStateOf(true)
-        private set
-
-    var isError by mutableStateOf(false)
-        private set
+    val state = MutableStateFlow(StartState())
 
     fun onCreate(context: Context, filename: String) {
         viewModelScope.launch {
-            isLoading = true
+            state.value = state.value.copy(
+                isLoading = true, isError = false
+            )
             if (cleanDatabasesUseCase.execute().isSuccess) {
-                insertSongsUseCase.execute(context, filename).fold(
-                    onSuccess = {
-                        val songsResult = getSongsUseCase.execute()
-                        val countCategories = songsResult.flatMap { it.categories }
-                            .groupingBy { it }
-                            .eachCount()
+                insertSongsUseCase.execute(context, filename).fold(onSuccess = {
+                    val songsResult = getSongsUseCase.execute()
+                    val countCategories =
+                        songsResult.flatMap { it.categories }.groupingBy { it }.eachCount()
 
-                        val countStages = songsResult.map { it.stage }
-                            .groupingBy { it }
-                            .eachCount()
+                    val countStages = songsResult.map { it.stage }.groupingBy { it }.eachCount()
 
-                        Category.entries.forEach {
-                            setCountCategoryUseCase.execute(it, countCategories[it] ?: 0)
-                        }
-
-                        Stage.entries.forEach {
-                            setCountStageUseCase.execute(it, countStages[it] ?: 0)
-                        }
-                    },
-                    onFailure = {
-                        isError = true
+                    Category.entries.forEach {
+                        setCountCategoryUseCase.execute(it, countCategories[it] ?: 0)
                     }
-                )
+
+                    Stage.entries.forEach {
+                        setCountStageUseCase.execute(it, countStages[it] ?: 0)
+                    }
+                }, onFailure = {
+                    state.value = state.value.copy(
+                        isError = true
+                    )
+                })
             } else {
-                isError = true
+                state.value = state.value.copy(
+                    isError = true
+                )
             }
 
-            isLoading = false
+            state.value = state.value.copy(
+                isLoading = false
+            )
         }
     }
 
