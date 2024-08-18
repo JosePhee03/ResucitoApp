2e5086068fdc9bffe60639d4cca3c0a86c40c76a
José Luis Gutierrez <joselugz03@gmail.com>
2024-08-17 21:59:57 -0300

    remove unusual dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,5 +86,4 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
-    implementation("me.onebone:toolbar-compose:2.3.5")
 }
