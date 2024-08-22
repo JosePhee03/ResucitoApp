bb2b0bba71825b74d05cf41d84db373f71a5d8d3
José Luis Gutierrez <joselugz03@gmail.com>
2024-08-22 09:52:58 -0300

    refactor imports

--- app/src/main/java/com/resucito/app/domain/usecase/song/GetSongUseCase.kt
+++ app/src/main/java/com/resucito/app/domain/usecase/song/GetSongUseCase.kt
@@ -1,12 +1,12 @@
 package com.resucito.app.domain.usecase.song
 
-import com.resucito.app.domain.repository.SongRepository
 import com.resucito.app.domain.model.Song
+import com.resucito.app.domain.repository.SongRepository
 import javax.inject.Inject
 
 class GetSongUseCase @Inject constructor(private val repository: SongRepository) {
 
-    suspend fun execute(songId: String):Song?{
+    suspend fun execute(songId: String): Song? {
         return repository.getSong(songId)
     }
 }
