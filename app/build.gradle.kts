dbd98d0f8f7974b2d61fcefb96948b4a849e17bd
José Luis Gutierrez <joselugz03@gmail.com>
2024-08-22 21:27:24 -0300

    add testing dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     testImplementation(libs.junit)
+    testImplementation(libs.junit.jupiter)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
@@ -85,5 +86,7 @@ dependencies {
 
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    testImplementation(libs.mockk)
+    implementation(libs.kotlinx.coroutines.test)
 
 }
