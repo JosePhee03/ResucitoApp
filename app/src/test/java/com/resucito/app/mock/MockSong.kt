3ab3ddd6fa7c2427cb7fd57e9e103d2f7d1a5068
José Luis Gutierrez <joselugz03@gmail.com>
2024-09-03 17:34:37 -0300

    add news mock from song entities and song dto

--- app/src/test/java/com/resucito/app/mock/MockSong.kt
+++ app/src/test/java/com/resucito/app/mock/MockSong.kt
@@ -1,5 +1,7 @@
 package com.resucito.app.mock
 
+import com.resucito.app.data.dto.SongDto
+import com.resucito.app.data.local.entity.SongEntity
 import com.resucito.app.domain.model.Category
 import com.resucito.app.domain.model.Song
 import com.resucito.app.domain.model.Stage
@@ -31,6 +33,67 @@ val MOCK_SONGS = listOf(
     )
 )
 
+val MOCK_SONG_ENTITIES = listOf(
+    SongEntity(
+        id = "1", title = "Song 1", subtitle = "Subtitle 1",
+        capo = 0,
+        page = 0,
+        tone = "",
+        lyric = "",
+        scale = "",
+        stage = Stage.LITURGY,
+        chords = listOf(""),
+        favorite = true,
+        categories = listOf(Category.EXIT)
+    ),
+    SongEntity(
+        id = "2", title = "Song 2", subtitle = "Subtitle 2",
+        capo = 0,
+        page = 0,
+        tone = "",
+        lyric = "",
+        scale = "",
+        stage = Stage.LITURGY,
+        chords = listOf(""),
+        favorite = true,
+        categories = listOf(Category.EXIT)
+    )
+)
+
+val MOCK_SONG_DTO = listOf(
+    SongDto(
+        id = "1", title = "Song 1", subtitle = "Subtitle 1",
+        capo = 0,
+        page = 0,
+        tone = "",
+        lyric = "",
+        scale = "",
+        stage = Stage.LITURGY,
+        chords = listOf(""),
+        categories = listOf(Category.EXIT)
+    ),
+    SongDto(
+        id = "2", title = "Song 2", subtitle = "Subtitle 2",
+        capo = 0,
+        page = 0,
+        tone = "",
+        lyric = "",
+        scale = "",
+        stage = Stage.LITURGY,
+        chords = listOf(""),
+        categories = listOf(Category.EXIT)
+    )
+)
+
+
 fun getMockSongs(): List<Song> {
     return MOCK_SONGS
+}
+
+fun getMockSongEntities(): List<SongEntity> {
+    return MOCK_SONG_ENTITIES
+}
+
+fun getMockSongsDto(): List<SongDto> {
+    return MOCK_SONG_DTO
 }
