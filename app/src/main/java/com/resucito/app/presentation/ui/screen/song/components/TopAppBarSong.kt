9daf682943d1e6f2022b54a87bf6aa5df93c4776
José Luis Gutierrez <joselugz03@gmail.com>
2024-09-12 11:32:20 -0300

    remove title from topbar

--- app/src/main/java/com/resucito/app/presentation/ui/screen/song/components/TopAppBarSong.kt
+++ app/src/main/java/com/resucito/app/presentation/ui/screen/song/components/TopAppBarSong.kt
@@ -4,7 +4,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
@@ -28,9 +27,7 @@ fun TopAppBarSong(
             containerColor = MaterialTheme.colorScheme.background,
             scrolledContainerColor = MaterialTheme.colorScheme.background,
         ),
-        title = {
-            Text(stringResource(R.string.song))
-        },
+        title = {},
         navigationIcon = {
             IconButton(onClick = { onBackNavigate() }) {
                 Icon(
